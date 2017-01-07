88db76d38efd15700b321c290f4862bd1a24f2d9
stackdump <myork@stackdump.com>
2017-01-07 13:06:18 -0800

    add manifest

--- setup.py
+++ setup.py
@@ -2,10 +2,19 @@ from setuptools import setup
 
 setup(
     name="bitwrap_pnml",
+    version="0.1.0",
+    author="Matthew York",
+    author_email="myork@stackdump.com",
+    description="A blockchain style eventsourcing service using cyclone.io and lmdb",
+    license='MIT',
+    keywords='PNML petri-net eventsourcing actor-model',
     packages=['bitwrap_pnml'],
     install_requires=['cyclone==1.1', 'lmdb==0.92', 'service-identity==16.0.0', 'txRDQ==0.2.14', 'ujson==1.35', 'xxhash==0.6.1', 'PyYAML==3.12'],
-    version="0.1.0",
+    long_description="A blockchain style eventsourcing service using cyclone.io and lmdb",
     url="http://getbitwrap.com",
-    author="Matthew York",
-    author_email="myork@stackdump.com"
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Database :: Database Engines/Servers",
+        "License :: OSI Approved :: MIT License"
+    ],
 )
