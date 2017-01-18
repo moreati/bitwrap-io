c35a7e3985452fdee423e30f4b58a9ba9001c9f6
stackdump <myork@stackdump.com>
2017-01-17 21:02:03 -0800

    only load spec once

--- bitwrap_pnml/api/spec.py
+++ bitwrap_pnml/api/spec.py
@@ -3,6 +3,8 @@ import cyclone.web
 import yaml
 import ujson as json
 
+with open(os.path.abspath(__file__ + '/../../../swagger.yml'), 'r') as stream:
+    SPEC = json.dumps(yaml.load(stream))
 
 class Resource(cyclone.web.RequestHandler):
     """ open api specification """
@@ -12,11 +14,8 @@ class Resource(cyclone.web.RequestHandler):
         self.set_header('Access-Control-Allow-Origin', os.environ.get('ALLOW_ORIGIN', '*'))
         self.set_header('Access-Control-Allow-Methods', 'GET')
         self.set_header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        self.set_header('Content-Type', 'application/json')
 
     def get(self):
         """ return swagger spec """
-        self.set_header('Content-Type', 'application/json')
-        with open(os.path.abspath(__file__ + '/../../../swagger.yml'), 'r') as stream:
-            spec = json.dumps(yaml.load(stream))
-
-        self.write(spec)
+        self.write(SPEC)
