4931ef3325045a9009b9ff896dc751e00accb6a5
stackdump <myork@stackdump.com>
2017-02-26 11:29:46 -0800

    WIP

--- bitwrap_io/test/test_event.py
+++ bitwrap_io/test/test_event.py
@@ -1,11 +1,10 @@
 """
-test calls against the json-rpc API
-
+test tic-tac-toe events with bitwrap_io
+using bitwrap schema file named "octothorpe"
 """
+
 import time
-import ujson as json
-import cyclone.httpclient
-from twisted.internet.defer import inlineCallbacks
+import json
 from twisted.internet import defer
 from bitwrap_io.test import ApiTest
 import bitwrap_io
@@ -14,67 +13,142 @@ from collections import OrderedDict
 
 class EventTest(ApiTest):
     """
-    Test tic-tac-toe state machine: #octothorpe
+    --------------------------
+    How 'octothorpe' state_machine
+     lets you play tic-tac-toe:
+    --------------------------
+    move('BEGIN')
+
+          |  |  
+        --+--+--
+          |  |  
+        --+--+--
+          |  |  
+
+    'BEGIN' sets up the empty board
+    --------------------------
+    move(<action>)
+
+    subsequent moves use a 3 char encoding
+
+        00|01|02
+        --+--+--
+        10|11|12
+        --+--+--
+        20|21|22
+
+    string: '[X|O][0-2][0-2]'
+    to specify token placment
+    --------------------------
+    move('X11')
+
+          |  |  
+        --+--+--
+          |X |  
+        --+--+--
+          |  |  
+
+    'X' takes middle center
+    --------------------------
+    move('O01')
+
+          |O |  
+        --+--+--
+          |X |  
+        --+--+--
+          |  |  
+
+    'O' takes top center
+
     """
 
     cli = ApiTest.client('api')
 
     def test_tic_tac_toe_sequence(self):
-        """ test a valid sequence of tic-tac-toe transformations """
-
+        """ test a sequence of tic-tac-toe transformations """
         d = defer.Deferred()
         oid = 'trial-' + time.time().__str__()
         schema = 'octothorpe'
-        seq = OrderedDict()
 
-        def test_response(res):
+        def test_response(res, code=200):
             """ test event response """
             print "\n", res.body
-            self.assertEquals(res.code, 200)
+            self.assertEquals(res.code, code)
             obj = json.loads(res.body)
             return obj
 
         def test_transform(res, action):
 
-            self.assertEqual(res['event']['error'], 0)
-            self.assertEqual(res['event']['state'], seq[action])
+            self.assertEqual(res['event']['error'], seq[action][1])
+            self.assertEqual(res['event']['state'], seq[action][0])
             print "\n", json.dumps(res)
 
-        def test_sequence():
+        def test_sequence(seq):
             """ generate a valid event stream """
 
-            # seq[<previous_action>] = <expected_state_vector>
-            seq['BEGIN'] = [0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0]
-            seq['X11']   = [1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0]
-            seq['O00']   = [0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0]
+            # seq[<previous_action>] = ([<expected_state_vector>], <error_code_int>)
+            seq['BEGIN'] = ([0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0], 0)
+            seq['X11']   = ([1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0], 0)
+            seq['O11']   = ([0, 0, 0, 2, 0, 1, 1, 1, 0, -1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0], 1) # invalid
+            seq['O00']   = ([0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0], 0)
 
             def tx(txt):
                 """ send an action event """
 
-                d.addCallback( lambda _: self.cli.transform({"schema": schema, "oid": oid, "action": txt}))
+                def _test(_):
+                    print "\n* tx-response\n", 
+                    return self.cli.transform({"schema": schema, "oid": oid, "action": txt})
+
+                d.addCallback( _test)
                 d.addCallback( lambda res: test_transform(res, txt))
 
             map(tx, seq)
 
-        def test_stream():
+        def test_stream(seq, count=0):
             """ test stream """
 
-            d.addCallback( lambda _: self.fetch('stream/octothorpe/'+ oid))
+            def _test(_):
+                print "\n* event-stream"
+                return self.fetch('stream/octothorpe/'+ oid)
+
+            d.addCallback( _test)
             d.addCallback( test_response )
-            d.addCallback( lambda obj: self.assertEquals(len(obj['events']), len(seq)))
+            d.addCallback( lambda obj: self.assertEquals(len(obj['events']), count))
 
         def test_head():
             """ test head event """
+            def _test(_):
+                print "\n* event -", 0
+                return self.fetch('head/octothorpe/'+ oid)
 
-            d.addCallback( lambda _: self.fetch('head/octothorpe/'+ oid))
+            d.addCallback( _test)
             d.addCallback( test_response )
 
-            d.addCallback( lambda obj: self.fetch('event/octothorpe/'+ obj['id']))
+        def test_prev(i):
+            """ test head event """
+
+            def _test(obj):
+              print "\n* event -", i
+              self.assertTrue(obj['event']['previous'])
+              return obj
+
+            d.addCallback( _test)
+            d.addCallback( lambda obj: self.fetch('event/octothorpe/'+ obj['event']['previous']))
             d.addCallback( test_response )
 
-        test_sequence()
+        def test_tail():
+            """ test prev is empty """
+            d.addCallback( lambda obj: self.assertFalse(obj['event']['previous']))
+            d.callback(None) # kick off chain of defers
+
+        seq = OrderedDict()
+        test_sequence(seq)
+
+        valid_event_count = len(filter(lambda x: x[1] == 0, seq.values()))
+        test_stream(seq, count=valid_event_count)
+
         test_head()
-        test_stream()
+        [ test_prev(i) for i in range(1, valid_event_count) ]
+        test_tail()
 
-        d.callback(None)
         return d
