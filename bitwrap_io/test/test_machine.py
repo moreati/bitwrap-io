11a7f556dc22b26fa2904c569487c5f533f4fff0
stackdump <myork@stackdump.com>
2017-02-26 23:01:03 -0800

    lmdb tests pass

--- bitwrap_io/test/test_machine.py
+++ bitwrap_io/test/test_machine.py
@@ -10,14 +10,15 @@ class MachineTest(ApiTest):
 
     cli = ApiTest.client('api')
 
-    def test_tic_tac_toe_sequence(self):
+    def test_machine_api(self):
         """ test a sequence of tic-tac-toe transformations """
         d = defer.Deferred()
         schema = 'octothorpe'
 
         def test_response(res, code=200):
             """ test event response """
-            print "\n", res.body
+            print "\n", res.request.url, res.code
+            #print "\n", res.body
             self.assertEquals(res.code, code)
 
         d.addCallback(lambda _: self.fetch('version'))
@@ -32,11 +33,11 @@ class MachineTest(ApiTest):
         d.addCallback(lambda _: self.fetch('pnml/%s.xml' % schema))
         d.addCallback(test_response)
 
-        #d.addCallback(lambda _: self.fetch('machine/%s.json' % schema))
-        #d.addCallback(test_response)
+        d.addCallback(lambda _: self.fetch('machine'))
+        d.addCallback(test_response)
 
-        #d.addCallback(lambda _: self.fetch('machines'))
-        #d.addCallback(test_response)
+        d.addCallback(lambda _: self.fetch('machine/%s' % schema))
+        d.addCallback(test_response)
 
         d.callback(None)
 
