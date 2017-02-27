6a62074b3a861ef2ee98ec1ad36adcb3f4b0392f
stackdump <myork@stackdump.com>
2017-02-26 23:17:06 -0800

    WIP

--- bitwrap_io/storage/sql.py
+++ bitwrap_io/storage/sql.py
@@ -52,7 +52,7 @@ def create_db(name='bitwrap', drop=False):
     conn.commit()
 
 
-class Storage(object):
+class Storage(base.Storage):
     """ dynamo Storage provider """
 
     def __init__(self, repo_name, state_machine):
@@ -64,45 +64,8 @@ class Storage(object):
             self.db = Datastore(repo_name, machine=self.state_machine)
             _POOL[repo_name] = self.db
 
-    def commit(self, req, dry_run=False):
-        """ execute transition and persist to storage on success """
 
-        oid = self.encode_key(req['oid'])
-        action = req['action']
-        transition = self.state_machine.machine['transitions'][action]
-
-        with self.db.cursor() as txn:
-
-            output = self.state_machine.vadd(self.db.state.vector(oid), transition['delta'])
-            prev = self.db.state.head(oid)
-
-            res = {
-                "oid": oid,
-                "action": action,
-                "state": output,
-                "payload": req.get('payload', {}),
-                "previous": prev,
-                "endpoint": req.get('endpoint', None),
-                "error": 0
-            }
-
-            if not self.state_machine.is_valid(output):
-                dry_run = True
-                res["error"] = 1
-                self.db.rollback()
-
-            if not dry_run:
-                eventid = xxhash.xxh64(self.serialize(res), seed=XX_SEED).hexdigest()
-                self.db.events.put(eventid, body=res, prev=prev)
-                self.db.state.put(oid, vector=output, head=eventid)
-                self.db.conn.commit()
-
-                return {'id': eventid, 'event': res}
-            else:
-                return {'id': None, 'event': res}
-
-
-class Datastore:
+class Datastore(object):
 
     def __init__(self, name, conn=None, machine=None, txn=None):
         if not conn:
@@ -120,7 +83,13 @@ class Datastore:
         self.txn = self.conn.cursor()
         return self.txn
 
-class State:
+    def commit(self):
+        pass
+
+    def rollback(self):
+        pass
+
+class State(object):
     """
     StateModel get and set state by oid
     """
