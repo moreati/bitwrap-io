74655dfada1f7c6caf71c72309bb5a0b820f75dc
stackdump <myork@stackdump.com>
2017-03-04 00:03:57 -0800

    bump

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:2.7.12
 
-ENV PROJECT_VERSION=0.1.1
+ENV PROJECT_VERSION=0.1.2
 
 WORKDIR /opt/bitwrap-io
 COPY . /opt/bitwrap-io/ 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bitwrap_io",
-    version="0.1.1",
+    version="0.1.2",
     author="Matthew York",
     author_email="myork@stackdump.com",
     description="A blockchain-style python eventstore w/ mysql or LMDB backend",
